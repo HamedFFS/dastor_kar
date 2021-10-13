f6a41c337cf8f03521786addef8dd066a7b6b635
HamedFFS <hamed.falahi7896@aut.ac.ir>
2021-10-13 13:47:44 +0330

    Init commit

---
+++ Work1.java
@@ -0,0 +1,27 @@
+
+public class Work1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!!!");
+        System.out.printf("\n");
+        System.out.println(4 + 2.5);
+        System.out.println(4 - 3.1);
+        System.out.println(4 / 3);
+        System.out.println(4 / 3.0);
+        System.out.println(3.6 / 1.7);
+        System.out.println(4 * 3.643253);
+        System.out.println(7 % 4);
+        System.out.println(7.3 % 3.1);
+        //fagat dar baghiMande, samt chap alamat tasir darad...
+        System.out.printf("\n");
+        System.out.println(-7 % 3);
+        System.out.println(7 % -3);
+        System.out.println(-7 % -3);
+        System.out.println(7 % 3);
+        //oulaviat alamatha...
+        System.out.printf("\n");
+        System.out.println(-2 * (3.5 + 2.1) / 4);
+
+    }
+}
